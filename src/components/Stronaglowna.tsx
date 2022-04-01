82ce830b66b44831931c8ae40ae6529f473be96c
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-04-01 20:58:38 +0200

    Update main in Stronaglowna.tsx

--- src/components/Stronaglowna.tsx
+++ src/components/Stronaglowna.tsx
@@ -20,10 +20,27 @@ function Stronaglowna() {
 			</header>
 
 			<main>
-				<h1>Strona główna</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tenetur ducimus cum quidem dignissimos consequuntur culpa quod iure error praesentium amet obcaecati natus, exercitationem dolor voluptatibus maiores laudantium mollitia facere.</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tenetur ducimus cum quidem dignissimos consequuntur culpa quod iure error praesentium amet obcaecati natus, exercitationem dolor voluptatibus maiores laudantium mollitia facere.</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias tenetur ducimus cum quidem dignissimos consequuntur culpa quod iure error praesentium amet obcaecati natus, exercitationem dolor voluptatibus maiores laudantium mollitia facere.</p>
+				<div id="stronaglowna">
+					<h1>Strona główna</h1>
+					<h2>
+						System planowania i zarządzania zadaniami w gospodarstwie rolnym to narzędzie, którego celem jest zapewnienie wsparcia w zarządzaniu zadaniami w gospodarstwie rolnym.
+					</h2>
+
+					<section className='FlexContainer'>
+						<div>
+							<h2>Planowanie i zarządzanie zadaniami</h2>
+							<p>
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur odio eaque non quaerat necessitatibus, velit facilis quis aut pariatur cupiditate, quasi iure omnis quibusdam repudiandae fuga dolor dolore numquam vero.
+							</p>
+						</div>
+						<div>
+							<h2>Monitoruj to co robią pracownicy</h2>
+							<p>
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur odio eaque non quaerat necessitatibus, velit facilis quis aut pariatur cupiditate, quasi iure omnis quibusdam repudiandae fuga dolor dolore numquam vero.
+							</p>
+						</div>
+					</section>
+				</div>
 			</main>
 		</div>
 	);
