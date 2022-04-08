e8a4cb42e96e8e96805589a11f4ea283cc6105d8
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-04-08 19:24:04 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,13 @@ import Stronaglowna from "./components/CzescGlowna/Stronaglowna";
 import Kontakt from "./components/CzescGlowna/Kontakt";
 import Login from "./components/CzescGlowna/Login";
 import Register from "./components/CzescGlowna/Register";
-import Pracownik from "./components/Pracownik/Pracownik";
+
 import Gospodarz from "./components/Gospodarz/Gospodarz";
 import TwojeKonto from "./components/Gospodarz/TwojeKonto";
 import Pracownicy from "./components/Gospodarz/Pracownicy";
 import ZadaniaGospodarz from "./components/Gospodarz/ZadaniaGospodarz";
+
+import Pracownik from "./components/Pracownik/Pracownik";
 import ZadaniaPracownik from "./components/Pracownik/ZadaniaPracownik";
 
 
