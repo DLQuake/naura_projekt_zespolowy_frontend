f45ae10f079454235efc711b58f3f965c919928a
Dominik Lewczyński <dominiklew200060@gmail.com>
2022-04-12 20:34:04 +0200

    Create Raporty.tsx

---
+++ src/components/Pracownik/Raporty.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import AsidePracownik from "../AsidePracownik";
+function Raporty() {
+    return (
+        <div className="App">
+            <AsidePracownik></AsidePracownik>
+            <main id="czescglownaPracownik">
+                <div className="raporty_pracownik">
+                    <h1>Raporty</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+                        doloremque, quidem, quisquam doloremque.
+                    </p>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default Raporty;
